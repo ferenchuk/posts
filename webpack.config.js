8b756a908a32b7922535b81ef4ac4881c5d625be
Ivan Ferenchuk <if@conciliolabs.com>
2019-07-31 03:13:08 +0300

    feat: project setup; webpack configuration;

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({template: './index.html'})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
